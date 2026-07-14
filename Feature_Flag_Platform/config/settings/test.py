28039a5f869f075db8cf12a2b54f9abb0e49dc04
Alihaghayegh <haghayeghali95@gmail.com>
2026-07-14 19:37:59 +0330

    02: Project stucture is ready and settings files and folders are seprated for development and production and requirements.txt added and local addresses to run project are modified

---
+++ Feature_Flag_Platform/config/settings/test.py
@@ -0,0 +1,11 @@
+from .base import *
+
+DEBUG = False
+
+PASSWORD_HASHERS = [
+
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+
+]
+
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
